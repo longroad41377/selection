3745bcc513114f2e41022de760520b5524d21ac0
41377 <41377@longroad.ac.uk>
2014-10-08 14:20:50 +0100

    Updated date program

--- data.py
+++ data.py
@@ -2,26 +2,35 @@ import re
 
 date = input("Enter date: ")
 
-match = re.search("^([0-9]+) ([0-9]+) ([0-9]+)$", date)
+match = re.match("(\d{1,2}) (\d{1,2}) (\d{2})$", date)
 
-day = int(match.group(1))
-month = int(match.group(2))
-year = int(match.group(3))
-
-if day % 10 == 1:
-    sufix = "st"
-elif day % 10 == 2:
-    sufix = "nd"
-elif day % 10 == 3:
-    sufix = "rd"
+if not match:
+    print("Invalid date")
 else:
-    sufix = "th"
+    day = int(match.group(1))
+    month = int(match.group(2))
+    year = int(match.group(3))
 
-day = str(day) + sufix
+    if day % 10 == 1:
+        sufix = "st"
+    elif day % 10 == 2:
+        sufix = "nd"
+    elif day % 10 == 3:
+        sufix = "rd"
+    else:
+        sufix = "th"
 
-monthnames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
-month = monthnames[month-1]
+    day = str(day) + sufix
 
-year = str(2000 + year)
+    monthnames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+    if month > 0 and month <= 12:
+        month = monthnames[month-1]
+    else:
+        month = "Invalid Month"
 
-print("{0} {1} {2}".format(day, month, year))
+    if year > 30:
+        year = str(1900 + year)
+    else:
+        year = str(2000 + year)
+
+    print("{0} {1} {2}".format(day, month, year))
